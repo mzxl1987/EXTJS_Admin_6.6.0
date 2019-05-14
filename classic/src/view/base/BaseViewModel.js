956e3897190e79c211ca0b4c8e3c1bcb4dea9d3f
mzxl1987 <448632824@qq.com>
2019-05-14 09:36:00 +0800

    1.动态菜单的生成

---
+++ classic/src/view/base/BaseViewModel.js
@@ -0,0 +1,13 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Mi.view.base.BaseViewModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.base',
+
+    data: {
+        preUrl: 'http://localhost:8080/',
+    }
+
+});
